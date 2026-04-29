3bfb1837149da9d5a89469ae70a1fe275988a41c
guirosalemalves <guii.alves_@hotmail.com>
2026-04-29 14:37:26 -0300

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  allowCypressEnv: false,
+  video: true,
 
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // pode deixar vazio por enquanto
     },
   },
 };
